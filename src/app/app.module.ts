18d2c7a1571e64c58ea1a49cbcc6fb0919a5dc60
Israel Vazquez <galathisrael@yahoo.com.mx>
2018-10-01 19:00:16 -0500

    First setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,58 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MyNavComponent } from './my-nav/my-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import {  MatToolbarModule,
+          MatButtonModule,
+          MatSidenavModule,
+          MatIconModule,
+          MatListModule,
+          MatGridListModule,
+          MatCardModule,
+          MatMenuModule,
+          MatTableModule,
+          MatPaginatorModule,
+          MatSortModule } from '@angular/material';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { MyDashboardComponent } from './my-dashboard/my-dashboard.component';
+import { FirstPageComponent } from './first-page/first-page.component';
+import { SecondPageComponent } from './second-page/second-page.component';
+import { ThirdPageComponent } from './third-page/third-page.component';
+import { MyTableComponent } from './my-table/my-table.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyNavComponent,
+    MyDashboardComponent,
+    FirstPageComponent,
+    SecondPageComponent,
+    ThirdPageComponent,
+    MyTableComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
